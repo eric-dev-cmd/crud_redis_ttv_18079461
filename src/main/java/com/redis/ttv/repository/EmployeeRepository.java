c343ae163315ba69c0f518a783e853f3d178b546
zunggzing <hello.trungvinh@gmail.com>
2021-11-08 16:13:15 +0700

    Update crud list

--- src/main/java/com/redis/ttv/repository/EmployeeRepository.java
+++ src/main/java/com/redis/ttv/repository/EmployeeRepository.java
@@ -16,6 +16,7 @@ public class EmployeeRepository {
     private RedisTemplate redisTemplate;
     private ListOperations listOperations;
     private SetOperations setOperations;
+
     private final static String EMPLOYEE_KEY = "EMPLOYEE";
 
     public EmployeeRepository(RedisTemplate redisTemplate) {
@@ -25,23 +26,33 @@ public class EmployeeRepository {
         this.listOperations = redisTemplate.opsForList();
     }
 
-    public void saveEmployee(Employee employee){
-
-        hashOperations.put("EMPLOYEE", employee.getId(), employee);
-    }
-    public List<Employee> findAll(){
-
-        return hashOperations.values("EMPLOYEE");
-    }
-    public Employee findById(Integer id){
-
-        return (Employee) hashOperations.get("EMPLOYEE", id);
+    public void saveEmployee(Employee employee) {
+        listOperations.leftPush("EMPLOYEE", employee);
+//        hashOperations.put("EMPLOYEE", employee.getId(), employee);
     }
 
-    public void update(Employee employee){
+    public List<Employee> findAll() {
+        return listOperations.range("EMPLOYEE", 0, listOperations.size("EMPLOYEE"));
+//        return hashOperations.values("EMPLOYEE");
+    }
+
+    public Employee findById(Integer id) {
+
+//        return (Employee) hashOperations.get("EMPLOYEE", id);
+        List<Employee> employees = this.findAll();
+        for (Employee employee : employees) {
+            if(employee.getId() == id)
+                return employee;
+        }
+        return null;
+    }
+
+    public void update(Employee employee) {
         saveEmployee(employee);
     }
-    public void delete(Integer id){
-        hashOperations.delete("EMPLOYEE", id);
+
+    public void delete(Integer id) {
+//        hashOperations.delete("EMPLOYEE", id);
+        listOperations.remove("EMPLOYEE", 1, findById(id));
     }
 }
