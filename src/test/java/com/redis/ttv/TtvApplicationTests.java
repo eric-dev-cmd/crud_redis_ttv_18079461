a6ab7015934cce36bb2a78e9bb71dbba733ef0ee
zunggzing <hello.trungvinh@gmail.com>
2021-11-08 10:34:19 +0700

    Inittial commit

---
+++ src/test/java/com/redis/ttv/TtvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redis.ttv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
